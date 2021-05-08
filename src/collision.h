17de6246355b7db6acd3f24c65a3078da0244478
Felipeek <felipeek@hotmail.com>
2021-05-08 18:27:32 -0300

    remove gjk+epa

--- src/collision.h
+++ src/collision.h
@@ -3,18 +3,6 @@
 #include "gm.h"
 #include "graphics.h"
 
-#define BOUNDING_SHAPE_NAME_MAX_SIZE 64
-
-typedef struct {
-  vec3 v;   // minkowski difference
-  vec3 sup; // original bshape vertex
-} Support_Point;
-
-typedef struct {
-  Support_Point simplex[4];
-  int current_index;
-} GJK_Support_List;
-
 typedef struct {
 	vec3 collision_point;
 	vec3 r_lc;
@@ -26,8 +14,5 @@ typedef struct {
 } Collision_Point;
 
 Collision_Point* collision_get_plane_cube_points(Entity* cube, r32 plane_y);
-
-boolean collision_gjk_collides(GJK_Support_List* sup_list, Bounding_Shape* b1, Bounding_Shape* b2);
-Collision_Point collision_epa(Support_Point* simplex, Bounding_Shape* b1, Bounding_Shape* b2);
 
 #endif
